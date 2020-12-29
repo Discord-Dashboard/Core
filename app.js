0fdd29ec1aff537c2fc5aa699b15cabc2556c5a1
Breathtake <62174194+breftejk@users.noreply.github.com>
2020-12-29 16:12:03 +0100

    config

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const db = require('quick.db');
 const Discord = require('discord.js');
+const config = require('./config.json')
 const bot = new Discord.Client();
 
 bot.login(config.token);
