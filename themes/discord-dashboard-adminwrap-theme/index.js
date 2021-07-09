a7c42d8623043ebc5ca9caa3d441201375ed2dc2
Breathtake <breftejk@gmail.com>
2021-07-09 20:48:18 +0200

    Delete themes/discord-dashboard-adminwrap-theme directory

--- themes/discord-dashboard-adminwrap-theme/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (config={}) => {
-    return {
-        viewsPath: require('path').join(__dirname, '/views'),
-        staticPath: require('path').join(__dirname, '/views/src'),
-        themeConfig: config
-    };
-};
