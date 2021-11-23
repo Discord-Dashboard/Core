e171fd5bb742eb5063a2a46b6a675c0ec79dd9d8
Breathtake <breftejk@gmail.com>
2021-11-23 20:41:26 +0100

    v1.5.5

---
+++ ModuleExportsFunctions/customPagesTypes.js
@@ -0,0 +1,23 @@
+module.exports = {
+    redirectToUrl: (endpoint, getDataFunction) => {
+        return {
+            type: "redirect",
+            endpoint: endpoint,
+            getEndpoint: getDataFunction
+        };
+    },
+    renderHtml: (endpoint, getDataFunction) => {
+        return {
+            type: "html",
+            endpoint: endpoint,
+            getHtml: getDataFunction
+        };
+    },
+    sendJson: (endpoint, getDataFunction) => {
+        return {
+            type: "json",
+            endpoint: endpoint,
+            getJson: getDataFunction
+        };
+    },
+}
