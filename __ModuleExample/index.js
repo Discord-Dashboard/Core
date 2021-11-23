8ee0685dc0bf904f0071a12bea725e90df55f852
Breathtake <breftejk@gmail.com>
2021-11-23 20:54:26 +0100

    v1.5.5

---
+++ __ModuleExample/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    app: ({app,config,themeConfig}) => {
+        app.get('/moduleexample', (req,res)=>{
+           res.send('ModuleExample: Hello World!');
+        });
+    },
+    server: ({io,server,config,themeConfig}) => {
+        const test = io.of('/moduleexample');
+        test.on('connection', () => {
+            console.log('ModuleExample socket.io connected.');
+        });
+    }
+}
