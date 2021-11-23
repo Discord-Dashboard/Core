b362798774d5d4144aad6d7f5169405c3552ed8b
Breathtake <breftejk@gmail.com>
2021-11-23 21:44:34 +0100

    v1.5.51

--- Routes/main.js
+++ Routes/main.js
@@ -14,8 +14,8 @@ module.exports = (app, config, themeConfig) => {
 
     router.get('/invite', (req, res) => {
         const scopes = config.invite.scopes || ["bot"];
-        if (req.params.g) {
-            return res.redirect(`https://discord.com/oauth2/authorize?client_id=${config.invite.clientId || config.bot.user.id}&scope=${scopes.join('%20')}&permissions=${config.invite.permissions || '0'}${config.invite.redirectUri ? `&response_type=code&redirect_uri=${config.invite.redirectUri}` : ''}${config.invite.otherParams || ''}&guild_id=${req.params.g}`);
+        if (req.query.g) {
+            return res.redirect(`https://discord.com/oauth2/authorize?client_id=${config.invite.clientId || config.bot.user.id}&scope=${scopes.join('%20')}&permissions=${config.invite.permissions || '0'}${config.invite.redirectUri ? `&response_type=code&redirect_uri=${config.invite.redirectUri}` : ''}${config.invite.otherParams || ''}&guild_id=${req.query.g}`);
         }
         res.redirect(`https://discord.com/oauth2/authorize?client_id=${config.invite.clientId || config.bot.user.id}&scope=${scopes.join('%20')}&permissions=${config.invite.permissions || '0'}${config.invite.redirectUri ? `&response_type=code&redirect_uri=${config.invite.redirectUri}` : ''}${config.invite.otherParams || ''}`);
     });
