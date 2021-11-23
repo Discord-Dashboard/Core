f7dabac845f465a2c92118bbe6ee313020b5f145
Breathtake <breftejk@gmail.com>
2021-11-23 20:50:25 +0100

    v1.5.5

---
+++ Routes/main.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const router = require('express').Router();
+
+module.exports = (app, config, themeConfig) => {
+    router.get('/', (req, res) => {
+        res.render('index', {
+            req: req,
+            themeConfig: req.themeConfig,
+            bot: config.bot
+        });
+    });
+
+    if (!config.invite) config.invite = {};
+
+    router.get('/invite', (req, res) => {
+        const scopes = config.invite.scopes || ["bot"];
+        if (req.params.g) {
+            return res.redirect(`https://discord.com/oauth2/authorize?client_id=${config.invite.clientId || config.bot.user.id}&scope=${scopes.join('%20')}&permissions=${config.invite.permissions || '0'}${config.invite.redirectUri ? `&response_type=code&redirect_uri=${config.invite.redirectUri}` : ''}${config.invite.otherParams || ''}&guild_id=${req.params.g}`);
+        }
+        res.redirect(`https://discord.com/oauth2/authorize?client_id=${config.invite.clientId || config.bot.user.id}&scope=${scopes.join('%20')}&permissions=${config.invite.permissions || '0'}${config.invite.redirectUri ? `&response_type=code&redirect_uri=${config.invite.redirectUri}` : ''}${config.invite.otherParams || ''}`);
+    });
+
+    config.supportServer ? null : config.supportServer = {};
+
+    router.get(`${config.supportServer.slash || '/support-server'}`, (req, res) => {
+        if (!config.supportServer.inviteUrl) return res.send({
+            error: true,
+            message: "No inviteUrl defined (discord-dashboard config.supportServer)."
+        });
+        if (!config.supportServer.inviteUrl.toLowerCase().startsWith('https://discord.gg/') && !config.supportServer.inviteUrl.toLowerCase().startsWith('https://discord.com/')) return res.send({
+            error: true,
+            message: "Invite url should start with 'https://discord.gg/' or 'https://discord.com/'."
+        });
+        res.redirect(config.supportServer.inviteUrl);
+    });
+
+    return router;
+}
