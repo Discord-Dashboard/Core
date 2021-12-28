3b5f9eceb6fbef712e3bc48f0119cee4451c4404
Breathtake <breftejk@gmail.com>
2021-12-28 20:32:56 +0100

    Update initExampleDash.js

--- InitFunctions/initExampleDash.js
+++ InitFunctions/initExampleDash.js
@@ -1,126 +1,134 @@
-module.exports = ({fileName, domain, port, token, clientSecret, clientId}) => {
+module.exports = ({fileName, domain, port, token, licenseId, clientSecret, clientId}) => {
     require('fs').writeFileSync(`${fileName}.js`, `
-const DBD = require('discord-dashboard');
-const CaprihamTheme = require('dbd-capriham-theme');
 
+/* --- DISCORD.JS CLIENT --- */
+
+const {Client, Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.login('${token}');
+
+/* --- DASHBOARD THEME & SETTINGS 'DATABASE'  --- */
+
+const DarkDashboard = require('dbd-dark-dashboard');
 let langsSettings = {};
-let currencyNames = {};
 
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
-client.login(${token});
+/* --- DASHBOARD --- */
+(async ()=>{
+    let DBD = require('../index');
+    await DBD.useLicense('${licenseId}');
+    DBD.Dashboard = DBD.UpdatedClass();
 
-const Dashboard = new DBD.Dashboard({
-    minimizedConsoleLogs: false,
-    port: ${port},
-    client: {
-        id: "${clientId}",
-        secret: "${clientSecret}"
-    },
-    redirectUri: \`${domain}${port == 80 || port == 443 ? '' : `:${port}`}/discord/callback\`,
-    domain: "${domain}",
-    bot: client,
-    ownerIDs: [],
-    theme: CaprihamTheme({
-        privacypolicy: {
-            websitename: "Assistants",
-            websiteurl: "${domain}",
-            supportemail: "support@" + "${domain}"
+    const Dashboard = new DBD.Dashboard({
+        port: ${port || 80},
+        client: {
+            id: '${clientId}',
+            secret: '${clientSecret}'
         },
-        websiteName: "Assistants",
-        iconURL: 'https://assistants.ga/ac_logo_v6.png',
-        index: {
-            card:{
-                title: "Assistants - The center of everything",
-                description: "Assistants Discord Bot management panel. Assistants Bot was created to give others the ability to do what they want. Just.<br>That's an example text.<br><br><b><i>Feel free to use HTML</i></b>",
-                image: "https://www.geeklawblog.com/wp-content/uploads/sites/528/2018/12/liprofile-656x369.png",
-            },
+        redirectUri: '${domain}/discord/callback',
+        domain: '${domain}',
+        bot: client,
+        theme: DarkDashboard({
             information: {
-                title: "Information",
-                description: "To manage your bot, go to the <a href='/manage'>Server Management page</a>.<br><br>For a list of commands, go to the <a href='/commands'>Commands page</a>.<br><br><b><i>You can use HTML there</i></b>"
+                createdBy: "Assistants Center",
+                websiteTitle: "Assistants Center",
+                websiteName: "Discord-Dashboard",
+                websiteUrl: "${domain}",
+                dashboardUrl: "${domain}",
+                supporteMail: "support@${domain}",
+                supportServer: "",
+                imageFavicon: "https://www.imidnight.ml/assets/img/logo-circular.png",
+                iconURL: "https://www.imidnight.ml/assets/img/logo-circular.png",
+                pagestylebg: "linear-gradient(to #2CA8FF, pink 0%, #155b8d 100%)",
+                main_color: "#2CA8FF",
+                sub_color: "#ebdbdb",
             },
-            feeds: {
-                title: "Feeds",
-                list: [
-                    {
-                        icon: "fa fa-user",
-                        text: "New user registered",
-                        timeText: "Just now",
-                        bg: "bg-light-info"
-                    },
-                    {
-                        icon: "fa fa-server",
-                        text: "Server issues",
-                        timeText: "3 minutes ago",
-                        bg: "bg-light-danger"
-                    }
-                ]
-            }
-        },
-        commands: {
-            pageTitle: "Commands",
-            table: {
-                title: "List",
-                subTitle: "All Assistants' commands",
-                list: 
-                [
-                    {
-                        commandName: "Test command",
-                        commandUsage: "prefix.test <arg> [op]",
-                        commandDescription: "Lorem ipsum dolor sth"
-                    },
-                    {
-                        commandName: "2nd command",
-                        commandUsage: "oto.nd <arg> <arg2> [op]",
-                        commandDescription: "Lorem ipsum dolor sth, arg sth arg2 stuff"
-                    }
-                ]
-            }
-        }
-    }),
-    settings: [
-        {
-            categoryId: 'setup',
-            categoryName: "Setup",
-            categoryDescription: "Setup your bot with default settings!",
-            categoryOptionsList: [
-                {
-                    optionId: 'lang',
-                    optionName: "Language",
-                    optionDescription: "Change bot's language easily",
-                    optionType: DBD.formTypes.select({"Polish": 'pl', "English": 'en', "French": 'fr'}),
-                    getActualSet: async ({guild}) => {
-                        return langsSettings[guild.id] || null;
-                    },
-                    setNew: async ({guild,newData}) => {
-                        langsSettings[guild.id] = newData;
-                        return; /*return {error: 'String'};*/
-                    }
+            invite: {
+                client_id: "[Client ID]",
+                redirectUri: "http://localhost:3000/close",
+                permissions: "8",
+            },
+            index: {
+                card: {
+                    category: "Discord-Dahsboard Panel - The center of everything",
+                    title: 'Welcome to the Discord-Dashboard where you can control the core features to the bot.',
+                    image: "https://i.imgur.com/axnP93g.png",
+                    footer: "Footer",
                 },
-            ]
-        },
-        {
-            categoryId: 'eco',
-            categoryName: "Economy",
-            categoryDescription: "Economy Module Settings",
-            categoryOptionsList: [
-                {
-                    optionId: 'currency_name',
-                    optionName: "Currency name",
-                    optionDescription: "Economy module Guild currency name",
-                    optionType: DBD.formTypes.input('Currency name', null, 10, false, true),
-                    getActualSet: async ({guild}) => {
-                        return currencyNames[guild.id] || null;
-                    },
-                    setNew: async ({guild,newData}) => {
-                        currencyNames[guild.id] = newData;
-                        return;
-                    }
+                information: {
+                    category: "Category",
+                    title: "Information",
+                    description: 'This bot and panel is currently a work in progress so contact me if you find any issues on discord.',
+                    footer: "Footer",
                 },
-            ]
-        },
-    ]
-});
+                feeds: {
+                    category: "Category",
+                    title: "Information",
+                    description: 'This bot and panel is currently a work in progress so contact me if you find any issues on discord.',
+                    footer: "Footer",
+                },
+            },
+            guilds: {
+                cardTitle: "Guilds",
+                cardDescription: "Here are all the guilds you currenly have permissions for:",
+            },
+            //If guildSettings is removed the text will not be visible.
+            guildSettings: {
+                cardTitle: "Guild Settings",
+                cardDescription: "Here you can manage all the settings for your guild:",
+            },
+            commands: {
+                categoryOne: {
+                    category: 'Starting Up',
+                    subTitle: 'All helpful commands',
+                    list: [
+                        {
+                            commandName: "Test command",
+                            commandUsage: "prefix.test <arg> [op]",
+                            commandDescription: "Lorem ipsum dolor sth",
+                            commandAlias: "Alias",
+                        },
+                        {
+                            commandName: "2nd command",
+                            commandUsage: "oto.nd <arg> <arg2> [op]",
+                            commandDescription: "Lorem ipsum dolor sth, arg sth arg2 stuff",
+                            commandAlias: "Alias",
+                        },
+                        {
+                            commandName: "Test command",
+                            commandUsage: "prefix.test <arg> [op]",
+                            commandDescription: "Lorem ipsum dolor sth",
+                            commandAlias: "Alias",
+                        },
+                    ],
+                },
+            },
+        }),
+        settings: [
+            {
+                categoryId: 'setup',
+                categoryName: "Setup",
+                categoryDescription: "Setup your bot with default settings!",
+                categoryOptionsList: [
+                    {
+                        optionId: 'lang',
+                        optionName: "Language",
+                        optionDescription: "Change bot's language easily",
+                        optionType: DBD.formTypes.select({"Polish": 'pl', "English": 'en', "French": 'fr'}),
+                        getActualSet: async ({guild}) => {
+                            return langsSettings[guild.id] || null;
+                        },
+                        setNew: async ({guild,newData}) => {
+                            langsSettings[guild.id] = newData;
+                            return;
+                        }
+                    },
+                ]
+            },
+        ]
+    });
 
-Dashboard.init();`)
+    Dashboard.init();
+})();
+
+       `)
 }
